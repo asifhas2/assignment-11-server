f7d08aad265ba8c7e8d4784a3f2af27e1cb55ae6
asifhas2 <ak1247942@gmail.com>
2026-05-13 00:35:46 +0600

    fix: payment feature update (remove secrets)

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-//middleware
+// stripe payment
+const Stripe = require("stripe");
 
+//middleware
+const stripe = Stripe(process.env.StRIPE_SECRET);
 app.use(express.json());
 app.use(cors());
 
@@ -27,70 +30,115 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const db = client.db("assignment_11");
-const userCollection = db.collection("users");
+    const db = client.db("assignment_11");
+    const userCollection = db.collection("users");
+    const paymentCollection = db.collection("payments");
 
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
 
-app.get('/users/:email', async (req, res) => {
+      const query = { email: email };
 
-    const email = req.params.email;
+      const result = await userCollection.findOne(query);
 
-    const query = { email: email };
+      res.send(result);
+    });
 
-    const result = await userCollection.findOne(query);
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "user";
+      user.plan = "free";
+      user.createAt = new Date();
+      const email = user.email;
 
-    res.send(result);
-
-});
-
-app.post("/users",async(req,res)=>{
-    const user = req.body;
-    user.role = "user";
-    user.plan="free"
-    user.createAt = new Date();
-    const email = user.email;
-
-    const findUser = await userCollection.findOne({ email });
-      if(findUser){
-        return res.send({message: 'user existed'})
+      const findUser = await userCollection.findOne({ email });
+      if (findUser) {
+        return res.send({ message: "user existed" });
       }
 
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-})
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
-app.patch("/users",async(req,res)=>{
+    app.patch("/users", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email };
+
+      const updateDoc = {
+        $set: {
+          plan: "premium",
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //payment serverApi
+
+    app.post("/create-checkout-session", async (req, res) => {
+      try {
+        const paymentInfo = req.body;
+
+        const amount = parseInt(paymentInfo.cost) * 100;
+
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ["card"],
+
+          line_items: [
+            {
+              price_data: {
+                currency: "usd",
+
+                product_data: {
+                  name: "Premium Plan",
+                },
+
+                unit_amount: amount,
+              },
+
+              quantity: 1,
+            },
+          ],
+
+          customer_email: paymentInfo.senderEmail,
+
+          mode: "payment",
+
+          success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+
+          cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancel`,
+        });
+
+        console.log(session);
+
+        res.send({
+          url: session.url,
+        });
+      } catch (error) {
+        console.log(error.message);
+
+        res.status(400).send({
+          error: error.message,
+        });
+      }
+    });
+
+  app.patch('/dashboard/:email', async (req, res) => {
    const email = req.params.email;
-   const filter = {email}
 
-   const updateDoc = {
-    $set:{
-        plan:'premium'
-    }
-   }
+   const result = await userCollection.updateOne(
+      { email },
+      {
+         $set: {
+            plan: "premium",
+            premiumTakenAt: new Date()
+         }
+      }
+   );
 
-   const result = await userCollection.updateOne(filter,updateDoc);
    res.send(result);
-})
-
-
-
-// const featuredCollection = db.collection("featuredLesson");
-
-
-// app.get('/featuredLesson',async(req,res)=>{
-//     const query ={}
-//     const cursor = featuredCollection.
-// })
-
-
-
-
-
-
-
-
-
+});
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
